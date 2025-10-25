11bc1268f707c86cff3901eca0fcc18f2bf4556c
Nirnay A P <Nirnayap7@gmail.com>
2025-10-25 12:46:23 +0530

    Update ssd1306.h

--- ssd1306.h
+++ ssd1306.h
@@ -1,7 +1,7 @@
 #ifndef SSD1306_H
 #define SSD1306_H
 
-#include "i2c_hal.h"
+#include "main.h"
 #include "bq76952.h"
 
 #define SSD1306_I2C_ADDR 0x3C
@@ -10,12 +10,12 @@
 #define SSD1306_BUFFER_SIZE (SSD1306_WIDTH * SSD1306_HEIGHT / 8)
 
 typedef struct {
-  I2C_HandleTypeDef *hi2c;
-  uint8_t buffer[SSD1306_BUFFER_SIZE];
+    I2C_HandleTypeDef *hi2c;
+    uint8_t buffer[SSD1306_BUFFER_SIZE];
 } ssd1306_t;
 
 void ssd1306_init(ssd1306_t *oled, I2C_HandleTypeDef *hi2c);
 void ssd1306_clear(ssd1306_t *oled);
-void ssd1306_update_bms_data(ssd1306_t *oled, int *cell_voltages, int current, int temp, int soc, bq76952_protection_t prot, uint8_t alert); // Changed to int
+void ssd1306_update_bms_data(ssd1306_t *oled, int *cell_voltages, int current, int temp, int soc, bq76952_protection_t prot, uint8_t alert);
 
-#endif /* SSD1306_H */
+#endif

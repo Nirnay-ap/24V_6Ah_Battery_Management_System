d31cb2da6184b99f4740cd0587339305dc74f2fe
Nirnay A P <Nirnayap7@gmail.com>
2025-10-25 12:45:07 +0530

    Update bq76952.h

--- bq76952.h
+++ bq76952.h
@@ -1,23 +1,23 @@
 #ifndef BQ76952_H
 #define BQ76952_H
 
-#include "i2c_hal.h"
+#include "main.h"
 #include <stdbool.h>
-#include <stdint.h>
 
 typedef struct {
-  uint8_t CELL_OV : 1;
-  uint8_t CELL_UV : 1;
+    uint8_t CELL_OV : 1;
+    uint8_t CELL_UV : 1;
+    uint8_t reserved : 6;
 } bq76952_protection_t;
 
 void bq76952_init(I2C_HandleTypeDef *hi2c);
 int bq76952_getCellVoltage(uint8_t cell);
 void bq76952_getAllCellVoltages(int *voltages);
 int bq76952_getCurrent(void);
-int bq76952_getInternalTemp(void); // Changed from float to int
+int bq76952_getInternalTemp(void);  // Changed to int
 bq76952_protection_t bq76952_getProtectionStatus(void);
 bool bq76952_isConnected(void);
 unsigned int bq76952_getDeviceNumber(void);
 uint8_t bq76952_HandleAlarm(void);
 
-#endif /* BQ76952_H */
+#endif
